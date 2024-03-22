d3fceaf6340a075fd2ad190e6ea1475518908ec1
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-03-22 10:32:58 +0530

    Demo commit

--- DemoProject/src/test/java/contacts_nivetha/ToPracticeCommitPushPull.java
+++ DemoProject/src/test/java/contacts_nivetha/ToPracticeCommitPushPull.java
@@ -3,7 +3,7 @@ package contacts_nivetha;
 public class ToPracticeCommitPushPull {
 
 	public static void main(String[] args) {
-		System.out.println("commit,push,pull");
+		System.out.println("commit,push,pull is done");
 
 	}
 
