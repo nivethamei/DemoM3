1f2c2ce0b0be8c72c13453865ddffe12845466ad
nivethamei <144530250+nivethamei@users.noreply.github.com>
2024-03-22 09:39:47 +0530

    First commit

---
+++ DemoProject/src/test/java/practise/ToLearnGitHub.java
@@ -0,0 +1,10 @@
+package practise;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class ToLearnGitHub {
+	public static void main(String[] args) {
+		System.out.println("learning github");
+	}
+
+}
